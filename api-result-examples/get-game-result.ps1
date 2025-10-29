159b66ee1d74e6f2861dca3520c65a084f02f9fc
philmist <philmist.j@gmail.com>
2025-10-29 12:26:03 +0900

    Add api result examples and scripts how to get these json.

---
+++ api-result-examples/get-game-result.ps1
@@ -0,0 +1,1 @@
+curl "https://open-api.bser.io/v1/games/53447003" -H "accept: application/json" -H "x-api-key: $Env:ER_DEV_APIKEY" > game-result.json
---
+++ api-result-examples/get-user-games.ps1
@@ -0,0 +1,1 @@
+curl -X GET "https://open-api.bser.io/v1/user/games/1733900" -H  "accept: application/json" -H  "x-api-key: $Env:ER_DEV_APIKEY" > user-games.json
