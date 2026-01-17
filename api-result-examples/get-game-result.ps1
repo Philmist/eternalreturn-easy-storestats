79ba43a10e76799660fe931150fc99f2f93db6a7
philmist <philmist.j@gmail.com>
2026-01-17 14:24:45 +0900

    chore: update API response examples

--- api-result-examples/get-game-result.ps1
+++ api-result-examples/get-game-result.ps1
@@ -1,1 +1,1 @@
-curl "https://open-api.bser.io/v1/games/56062089" -H "accept: application/json" -H "x-api-key: $Env:ER_DEV_APIKEY" > game-result.json
+curl "https://open-api.bser.io/v1/games/56387685" -H "accept: application/json" -H "x-api-key: $Env:ER_DEV_APIKEY" > game-result.json
