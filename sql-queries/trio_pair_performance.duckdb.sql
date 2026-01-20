86c9df41c911bbfbc0811dd325f0b67f7e930482
philmist <philmist.j@gmail.com>
2026-01-21 00:54:45 +0900

    chore: Change trio_pair_performance query contains mmr

--- sql-queries/trio_pair_performance.duckdb.sql
+++ sql-queries/trio_pair_performance.duckdb.sql
@@ -6,9 +6,9 @@ WITH team AS (
     p.game_id,
     p.team_number,
     m.matching_mode,
-    array_sort(LIST(p.character_num)) AS characters_id,
-    array_sort(LIST(c.name)) AS characters_name,
-    list_count(LIST(p.character_num)) AS players,
+    array_sort (LIST (p.character_num)) AS characters_id,
+    array_sort (LIST (c.name)) AS characters_name,
+    list_count (LIST (p.character_num)) AS players,
     MAX(p.mmr_gain) AS mmr,
     MAX(p.victory) AS team_victory,
     MIN(p.game_rank) AS team_rank
@@ -17,12 +17,12 @@ WITH team AS (
     JOIN db.matches AS m ON p.game_id = m.game_id
     JOIN db.characters AS c ON c.character_code = p.character_num
   WHERE
-    -- matching_mode, mode_name = [(2, normal) | (3, ranked) | (6, cobalt) | (8, union)]
-    m.matching_mode IN (2, 3)
-  GROUP BY
-    p.game_id,
-    p.team_number,
-    m.matching_mode
+  -- matching_mode, mode_name = [(2, normal) | (3, ranked) | (6, cobalt) | (8, union)]
+  m.matching_mode IN (2, 3)
+GROUP BY
+  p.game_id,
+  p.team_number,
+  m.matching_mode
 ),
 pairs AS (
   SELECT
@@ -32,22 +32,25 @@ pairs AS (
     team_victory,
     team_rank,
     mmr,
-    list_extract(characters_id, i + 1) AS character_id_a,
-    list_extract(characters_id, j + 1) AS character_id_b,
-    list_extract(characters_name, i + 1) AS character_name_a,
-    list_extract(characters_name, j + 1) AS character_name_b
+    list_extract (characters_id, i + 1) AS character_id_a,
+    list_extract (characters_id, j + 1) AS character_id_b,
+    list_extract (characters_name, i + 1) AS character_name_a,
+    list_extract (characters_name, j + 1) AS character_name_b
   FROM
     team
-    CROSS JOIN range(0, list_count(characters_id) - 1) AS i(i)
-    CROSS JOIN range(i + 1, list_count(characters_id)) AS j(j)
+    CROSS JOIN RANGE (0,
+      list_count (characters_id) - 1) AS i (i)
+    CROSS JOIN RANGE (i + 1,
+      list_count (characters_id)) AS j (j)
   WHERE
-    -- matching_mode, players = [(2, 3) | (3, 3) | (6, 4) | (8, 3)]
-    players = 3
+  -- matching_mode, players = [(2, 3) | (3, 3) | (6, 4) | (8, 3)]
+  players = 3
 )
 SELECT
-  [character_id_a, character_id_b] AS characters_id,
-  [character_name_a, character_name_b] AS characters_name,
+[character_id_a, character_id_b] AS characters_id,
+[character_name_a, character_name_b] AS characters_name,
   COUNT(*) AS matches_played,
+  MAX(mmr) AS mmr,
   ROUND(AVG(team_rank), 2) AS avg_rank,
   ROUND(SUM(
       CASE WHEN team_victory = 1 THEN
@@ -57,13 +60,17 @@ SELECT
       END)::double / COUNT(*), 4) AS win_rate
 FROM
   pairs
+WHERE
+  mmr >= 0
 GROUP BY
   characters_id,
   characters_name
 HAVING
-  matches_played >= 15 -- samples
+  matches_played >= 20 -- samples
 ORDER BY
-  avg_rank ASC,
   win_rate DESC,
+  avg_rank ASC,
+  mmr DESC,
   matches_played DESC
-LIMIT 50;
+LIMIT 20;
+
